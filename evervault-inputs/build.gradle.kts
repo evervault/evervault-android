5106169f2887a46a44a85c0aa1434bea5588b8f8
Eoin Boylan <jb.eoin@gmail.com>
2023-10-16 14:17:19 +0100

    version v1.5

--- evervault-inputs/build.gradle.kts
+++ evervault-inputs/build.gradle.kts
@@ -78,7 +78,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.evervault.sdk"
             artifactId = "evervault-inputs"
-            version = "1.4"
+            version = "1.5"
 
             afterEvaluate {
                 from(components["release"])
