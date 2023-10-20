bd48f89fc5ff97db567dd4aee915e9909c50b5fa
Adnan Jelic <adnan.jelic@gmail.com>
2023-10-20 10:49:01 +0200

    Created the new PaymentCardInputComponent with its samples and docs

---
+++ evervault-inputs/src/main/java/com/evervault/sdk/input/ui/PaymentCardInputComponentSamples.kt
@@ -0,0 +1,81 @@
+package com.evervault.sdk.input.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+internal fun PaymentCardInputComponentDefaultLayout() {
+    PaymentCardInputComponent()
+}
+
+@Preview
+@Composable
+internal fun PaymentCardInputComponentRowsLayout() {
+    PaymentCardInputComponent(content = rowsPaymentCardInputLayout())
+}
+
+@Preview
+@Composable
+fun PaymentCardInputComponentCustomLayout() {
+    val modifier = Modifier
+        .background(color = Color.Blue, RoundedCornerShape(16.dp))
+        .padding(24.dp)
+
+    PaymentCardInputComponent(
+        textStyle = TextStyle.Default.copy(color = Color.Yellow),
+        placeholderTextStyle = TextStyle.Default.copy(color = Color.LightGray),
+        modifier = Modifier
+            .background(color = Color.Blue, RoundedCornerShape(16.dp))
+            .padding(24.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+        ) {
+            Column {
+                CardNumberField(
+                    options = PaymentCardInputScope.TextFieldOptions { TextStyle.Default.copy(color = Color.Red) },
+                    placeholder = "Custom card placeholder",
+                    modifier = modifier
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Column(
+                    modifier = Modifier.weight(0.5f)
+                ) {
+                    ExpiryField(
+                        options = PaymentCardInputScope.TextFieldOptions {
+                            TextStyle.Default.copy(color = Color.Yellow)
+                        },
+                        placeholder = "Custom expiry placeholder",
+                        modifier = modifier
+                    )
+                }
+                Column(
+                    modifier = Modifier.weight(0.5f)
+                ) {
+                    CVCField(
+                        options = PaymentCardInputScope.TextFieldOptions(),
+                        placeholder = "Custom CVC placeholder",
+                        modifier = modifier
+                    )
+                }
+            }
+        }
+    }
+}
