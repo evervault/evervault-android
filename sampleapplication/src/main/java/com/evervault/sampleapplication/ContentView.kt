aaa66c7591839c56daa9b7f294b8f1bfff905ba4
Adnan Jelic <adnan.jelic@gmail.com>
2023-10-25 23:38:32 +0200

    Added navigation to the new customizable Composable layout example

--- sampleapplication/src/main/java/com/evervault/sampleapplication/ContentView.kt
+++ sampleapplication/src/main/java/com/evervault/sampleapplication/ContentView.kt
@@ -3,6 +3,7 @@ package com.evervault.sampleapplication
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
@@ -10,6 +11,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -48,6 +50,11 @@ fun ContentView(navController: NavController) {
                 ListItem(
                     headlineContent = { Text(text = "Custom Layout") },
                     modifier = Modifier.clickable { navController.navigate("CreditCardInputViewCustomStyle") }
+                )
+                Divider(Modifier.padding(horizontal = 16.dp))
+                ListItem(
+                    headlineContent = { Text(text = "Custom Layout with Composables") },
+                    modifier = Modifier.clickable { navController.navigate("CreditCardInputViewCustomComposables") }
                 )
             }
             item {
