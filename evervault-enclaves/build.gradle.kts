0c1a81510be46a7c040dbbc6e556454bd6b989c4
Eoin Boylan <jb.eoin@gmail.com>
2024-08-27 09:57:21 +0100

    feat: remove duplicate dependancy from android (#61)
    * feat: remove duplicate dependancy from android
    
    * chore: add changeset

--- evervault-enclaves/build.gradle.kts
+++ evervault-enclaves/build.gradle.kts
@@ -67,7 +67,6 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.5.1")
     implementation("net.java.dev.jna:jna:5.7.0@aar")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    implementation("junit:junit:4.12")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
