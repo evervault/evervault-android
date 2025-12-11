adff369cb5d29b096471c3d586347b723f1b22e6
Matt Paletta <matt@evervault.com>
2025-12-11 14:14:20 +0000

    Update dependencies

--- evervault-core/build.gradle.kts
+++ evervault-core/build.gradle.kts
@@ -59,8 +59,8 @@ android {
 dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
-//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.0.0")
 
     implementation(kotlin("stdlib-common"))
